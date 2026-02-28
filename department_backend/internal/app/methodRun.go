1daa7b1ed51e60f422e98c9ff3ca56c8d98816ce
Carlos Fabian Corrales Zapata <fabianzapata98@outlook.com>
2026-02-28 15:45:57 -0500

    create port

--- department_backend/internal/app/methodRun.go
+++ department_backend/internal/app/methodRun.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/AppBlitz/department_backend/internal/repository"
 	"github.com/AppBlitz/department_backend/internal/service"
@@ -15,7 +16,9 @@ func Run(db *sql.DB) {
 	repo := repository.NewDepartmentRepository(db)
 	servi := service.NewDepartmentService(repo)
 	trans := https.NewDepartmentHandler(servi)
-	erro := http.ListenAndServe(":8080", https.AllHandlers(trans))
+
+	portListen := os.Getenv("PORT_LISTEN")
+	erro := http.ListenAndServe(":"+portListen, https.AllHandlers(trans))
 	if erro != nil {
 		log.Fatal("Erro in server: ", erro)
 	}
