20e29fa8df04a95c612e14c7b97fc4878c69ee29
Carlos Fabian Corrales Zapata <fabianzapata98@outlook.com>
2026-03-19 22:22:35 -0500

    cassandra

---
+++ microservice-logs/src/model/log_model.js
@@ -0,0 +1,54 @@
+class Log {
+  constructor(id_employee, name_employee, email_employee, department_id, date_enter) {
+    this._id_employee = id_employee;
+    this._name_employee = name_employee;
+    this._email_employee = email_employee;
+    this._department_id = department_id;
+    this._date_enter = date_enter;
+  }
+
+  get id_employee() {
+    return this._id_employee;
+  }
+
+  get name_employee() {
+    return this._name_employee;
+  }
+
+  get email_employee() {
+    return this._email_employee;
+  }
+
+  get department_id() {
+    return this._department_id;
+  }
+
+  get date_enter() {
+    return this._date_enter;
+  }
+
+  set id_employee(value) {
+    this._id_employee = value;
+  }
+
+  set name_employee(value) {
+    this._name_employee = value;
+  }
+
+  set email_employee(value) {
+    this._email_employee = value;
+  }
+
+  set department_id(value) {
+    this._department_id = value;
+  }
+
+  set date_enter(value) {
+    this._date_enter = value;
+  }
+
+
+}
+
+
+export { Log }
--- microservice-logs/src/services/service_logs.js
+++ microservice-logs/src/services/service_logs.js
@@ -1,4 +1,4 @@
-import { add_logs_save_employee } from "../repositories/cassandra.js"
+import { add_logs_save_employee, search_all } from "../repositories/cassandra.js"
 
 function insert_log_save_employee(log_employee_save) {
   const employee_log_save = {
@@ -9,5 +9,10 @@ function insert_log_save_employee(log_employee_save) {
     date_enter: log_employee_save.date_enter
   }
   add_logs_save_employee(JSON.stringify(employee_log_save));
+}
+
+async function search_all_notifications() {
+  const data = await search_all()
+  return data;
 }
 export { insert_log_save_employee }
