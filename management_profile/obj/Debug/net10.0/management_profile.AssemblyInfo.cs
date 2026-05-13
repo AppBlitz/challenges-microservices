3ae3b0f02ce9701113cf2bf6eb87ceae7251e3ac
KJahn26 <130727858+KJahn26@users.noreply.github.com>
2026-05-13 08:22:29 -0500

    bla bla

--- management_profile/obj/Debug/net10.0/management_profile.AssemblyInfo.cs
+++ management_profile/obj/Debug/net10.0/management_profile.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("management_profile")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c69173a5f6f1e188253bc2dcdf58a077a7a137ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e62513233e9eaa1e63dd5ffc8911b7370ccc8cbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("management_profile")]
 [assembly: System.Reflection.AssemblyTitleAttribute("management_profile")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
