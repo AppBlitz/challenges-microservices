f1132322e25c6b28062fcb302545474cf8b2748e
KJahn26 <130727858+KJahn26@users.noreply.github.com>
2026-05-18 22:00:43 -0500

    sonarqube 16

--- employee-backend/build.gradle.kts
+++ employee-backend/build.gradle.kts
@@ -4,8 +4,7 @@ plugins {
 	id("org.springframework.boot") version "4.0.2"
 	id("io.spring.dependency-management") version "1.1.7"
 	id("jacoco")
-	// id ("org.sonarqube") version "4.4.1.3373"
-	id ("org.sonarqube") version "5.1.0.4882"
+	id ("org.sonarqube") version "4.4.1.3373"
 }
 
 group = "com.employee"
