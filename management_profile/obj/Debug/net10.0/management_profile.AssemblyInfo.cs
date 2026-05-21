1ef8731b0eae645c1383c3a975fe53e7a5f4548a
KJahn26 <130727858+KJahn26@users.noreply.github.com>
2026-05-21 13:31:07 -0500

    pipelines 1

--- management_profile/obj/Debug/net10.0/management_profile.AssemblyInfo.cs
+++ management_profile/obj/Debug/net10.0/management_profile.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("management_profile")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5eb1b2f5841877835a9838789bb562eb61dd3732")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+838b2f71f874009e035857ce886bb3c014fc8767")]
 [assembly: System.Reflection.AssemblyProductAttribute("management_profile")]
 [assembly: System.Reflection.AssemblyTitleAttribute("management_profile")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
